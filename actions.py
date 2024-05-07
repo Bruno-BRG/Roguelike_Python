a9d5fa76069a650c97b2415661ef3a24754114dd
Frykits <88166479+Frykits@users.noreply.github.com>
2024-05-07 17:54:32 -0300

    part2Completa

--- actions.py
+++ actions.py
@@ -1,11 +1,34 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from engine import Engine
+    from entity import Entity
+
 class Action:
-    pass
+    def perform(self, engine: Engine, entity: Entity) -> None:
+        raise NotImplementedError()
 
 class EscapeAction(Action):
-    pass
+    def perform(self, engine: Engine, entity: Entity) -> None:
+        raise SystemExit()
+
 
 class MovementAction(Action):
     def __init__(self, dx: int, dy: int):
-        super().__init__() 
+        super().__init__()
+
         self.dx = dx
         self.dy = dy
+
+    def perform(self, engine: Engine, entity: Entity) -> None:
+        dest_x = entity.x + self.dx
+        dest_y = entity.y + self.dy
+
+        if not engine.game_map.in_bounds(dest_x, dest_y):
+            return
+
+        if not engine.game_map.tiles["walkable"][dest_x, dest_y]:
+            return
+
+        entity.move(self.dx, self.dy)
