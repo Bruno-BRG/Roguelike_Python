198656fc87048ea6c0eccfb3a6568a747f62392e
Bruno Guimarães <88166479+Frykits@users.noreply.github.com>
2024-05-10 15:20:42 -0300

    Add files via upload

--- engine.py
+++ engine.py
@@ -1,33 +1,72 @@
-from typing import Set, Iterable, Any
+from __future__ import annotations
 
-from tcod.context import Context
+import lzma
+import pickle
+from typing import TYPE_CHECKING
+
 from tcod.console import Console
+from tcod.map import compute_fov
 
-from entity import Entity
-from game_map import GameMap
-from input_handlers import EventHandler
+import exceptions
+from message_log import MessageLog
+import render_functions
+
+if TYPE_CHECKING:
+    from entity import Actor
+    from game_map import GameMap, GameWorld
+
 
 class Engine:
-    def __init__(self, entities: Set[Entity], event_handler: EventHandler, game_map: GameMap, player: Entity):
-        self.entities = entities
-        self.event_handler = event_handler
-        self.game_map = game_map
+    game_map: GameMap
+    game_world: GameWorld
+
+    def __init__(self, player: Actor):
+        self.message_log = MessageLog()
+        self.mouse_location = (0, 0)
         self.player = player
 
-    def handle_events(self, events: Iterable[Any]) -> None:
-        for event in events:
-            action = self.event_handler.dispatch(event)
+    def handle_enemy_turns(self) -> None:
+        for entity in set(self.game_map.actors) - {self.player}:
+            if entity.ai:
+                try:
+                    entity.ai.perform()
+                except exceptions.Impossible:
+                    pass  # Ignore impossible action exceptions from AI.
 
-            if action is None:
-                continue
-            
-            action.perform(self, self.player)
-           
-    def render(self, console: Console, context: Context) -> None:
-        self.game_map.render(console)  
-        
-        for entity in self.entities:
-            console.print(entity.x, entity.y, entity.char, fg=entity.color)
+    def update_fov(self) -> None:
+        """Recompute the visible area based on the players point of view."""
+        self.game_map.visible[:] = compute_fov(
+            self.game_map.tiles["transparent"],
+            (self.player.x, self.player.y),
+            radius=8,
+        )
+        # If a tile is "visible" it should be added to "explored".
+        self.game_map.explored |= self.game_map.visible
 
-        context.present(console)
-        console.clear()
+    def render(self, console: Console) -> None:
+        self.game_map.render(console)
+
+        self.message_log.render(console=console, x=21, y=45, width=40, height=5)
+
+        render_functions.render_bar(
+            console=console,
+            current_value=self.player.fighter.hp,
+            maximum_value=self.player.fighter.max_hp,
+            total_width=20,
+        )
+
+        render_functions.render_dungeon_level(
+            console=console,
+            dungeon_level=self.game_world.current_floor,
+            location=(0, 47),
+        )
+
+        render_functions.render_names_at_mouse_location(
+            console=console, x=21, y=44, engine=self
+        )
+
+    def save_as(self, filename: str) -> None:
+        """Save this Engine instance as a compressed file."""
+        save_data = lzma.compress(pickle.dumps(self))
+        with open(filename, "wb") as f:
+            f.write(save_data)
--- main.py
+++ main.py
@@ -1,32 +1,30 @@
 #!/usr/bin/env python3
+import traceback
+
 import tcod
 
-from engine import Engine
-from entity import Entity
-from game_map import GameMap
-from input_handlers import EventHandler
+import color
+import exceptions
+import setup_game
+import input_handlers
+
+
+def save_game(handler: input_handlers.BaseEventHandler, filename: str) -> None:
+    """If the current event handler has an active Engine then save it."""
+    if isinstance(handler, input_handlers.EventHandler):
+        handler.engine.save_as(filename)
+        print("Game saved.")
 
 
 def main() -> None:
     screen_width = 80
     screen_height = 50
 
-    map_width = 80
-    map_height = 45
-
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
 
-    event_handler = EventHandler()
-
-    player = Entity(int(screen_width / 2), int(screen_height / 2), "@", (255, 255, 255))
-    npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), "@", (255, 255, 0))
-    entities = {npc, player}
-
-    game_map = GameMap(map_width, map_height)
-
-    engine = Engine(entities=entities, event_handler=event_handler, game_map=game_map, player=player) 
+    handler: input_handlers.BaseEventHandler = setup_game.MainMenu()
 
     with tcod.context.new_terminal(
         screen_width,
@@ -35,15 +33,33 @@ def main() -> None:
         title="Yet Another Roguelike Tutorial",
         vsync=True,
     ) as context:
-        root_console = tcod.console.Console(screen_width, screen_height, order="F")
-        while True:
-            engine.render(console=root_console, context=context) 
+        root_console = tcod.Console(screen_width, screen_height, order="F")
+        try:
+            while True:
+                root_console.clear()
+                handler.on_render(console=root_console)
+                context.present(root_console)
 
-            events = tcod.event.wait()
+                try:
+                    for event in tcod.event.wait():
+                        context.convert_event(event)
+                        handler = handler.handle_events(event)
+                except Exception:  # Handle exceptions in game.
+                    traceback.print_exc()  # Print error to stderr.
+                    # Then print the error to the message log.
+                    if isinstance(handler, input_handlers.EventHandler):
+                        handler.engine.message_log.add_message(
+                            traceback.format_exc(), color.error
+                        )
+        except exceptions.QuitWithoutSaving:
+            raise
+        except SystemExit:  # Save and quit.
+            save_game(handler, "savegame.sav")
+            raise
+        except BaseException:  # Save on any other unexpected exception.
+            save_game(handler, "savegame.sav")
+            raise
 
-            root_console.clear()
-
-            engine.handle_events(events) 
 
 if __name__ == "__main__":
     main()
